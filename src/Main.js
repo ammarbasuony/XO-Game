bd63369e10965acf1acec393cd809449e1f505fe
Ammar Yaser <ammaryaser.dev@gmail.com>
2022-04-01 00:04:20 +0200

    Setup Environment

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="bg-slate-900 h-screen justify-center align-middle">
+      
+    </div>
+  );
+}
+
+export default Main;
